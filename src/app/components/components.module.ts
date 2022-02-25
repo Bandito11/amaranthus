2aa3265fe445dd167062cab69cdfdf992f7653ed
Esteban Morales <red_bandit_11@hotmail.com>
2022-02-25 00:35:08 -0400

    adding new component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,26 +6,25 @@ import { GobackComponent } from './goback/goback.component';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { CancelComponent } from './cancel/cancel.component';
-
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
-    declarations: [
-        LoaderComponent,
-        GobackComponent,
-        CalendarComponent,
-        CancelComponent,
-        MenuButtonComponent
-    ],
-    imports: [
-        IonicModule,
-        CommonModule,
-    ],
+  declarations: [
+    LoaderComponent,
+    GobackComponent,
+    CalendarComponent,
+    CancelComponent,
+    MenuButtonComponent,
+    UserItemComponent,
+  ],
+  imports: [IonicModule, CommonModule],
   exports: [
-        LoaderComponent,
-        GobackComponent,
-        CalendarComponent,
-        CancelComponent,
-        MenuButtonComponent
-    ]
+    LoaderComponent,
+    GobackComponent,
+    CalendarComponent,
+    CancelComponent,
+    MenuButtonComponent,
+    UserItemComponent,
+  ],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
