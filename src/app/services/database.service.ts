6cd40e9d029e7697a890af59ca8059deebce4504
Esteban Morales <red_bandit_11@hotmail.com>
2022-03-03 02:15:08 -0400

    added methods for adding attendance, notes

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ICalendar, IRecord, IStudent } from '../common/models';
+import { ICalendar, INote, IRecord, IStudent } from '../common/models';
 import { AmaranthusDBProvider } from './amaranthus-db/amaranthus-db';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatabaseService {
+  constructor(private db: AmaranthusDBProvider) {}
 
-  constructor(private db: AmaranthusDBProvider) { }
-
-  async getAllActiveStudents(date: ICalendar): Promise<(IStudent & IRecord)[]>{
+  async getAllActiveStudents(date: ICalendar): Promise<(IStudent & IRecord)[]> {
     const students = await this.db.getAllActiveStudents(date);
     return students;
+  }
+
+  addAttendance(opts: { date: ICalendar; id: string; event?: string }) {
+    this.db.addAttendance({ date: opts.date, id: opts.id });
+    return true;
+  }
+
+  addAbsence(opts: { date: ICalendar; id: string; event?: string }) {
+    this.db.addAbsence({ date: opts.date, id: opts.id });
+    return true;
+  }
+
+  addNotes(note: INote) {
+    this.db.insertNotes(note);
   }
 }
