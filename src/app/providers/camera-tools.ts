d6b37469fcb76e4803e65ff5e2eb408768352acf
Esteban Morales <red_bandit_11@hotmail.com>
2022-04-14 23:25:58 -0400

    added a check for errors

--- src/app/providers/camera-tools.ts
+++ src/app/providers/camera-tools.ts
@@ -6,16 +6,30 @@ import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
  * @returns {Promise<Photo>}
  */
 export async function takePicture() {
-  const image = await Camera.getPhoto({
-    quality: 90,
-    allowEditing: true,
-    resultType: CameraResultType.Uri,
-    source: CameraSource.Prompt,
-  });
+  try {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Prompt,
+    });
 
-  // image.webPath will contain a path that can be set as an image src.
-  // You can access the original file using image.path, which can be
-  // passed to the Filesystem API to read the raw data of the image,
-  // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-  return image;
+    return image;
+  } catch (error) {
+    let permissions = await Camera.checkPermissions();
+    if (permissions.camera === 'denied') {
+      permissions = await Camera.requestPermissions();
+      if (permissions.camera === 'denied') {
+        throw 'Camera permissions was denied';
+      }
+    }
+
+    if (permissions.photos === 'denied') {
+      permissions = await Camera.requestPermissions();
+      if (permissions.photos === 'denied') {
+        throw 'Photos permissions was denied';
+      }
+    }
+    return this.takePicture();
+  }
 }
