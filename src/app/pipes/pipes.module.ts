93a76913c3d77a6c96fbe66b4868b554494630ba
Bandito11 <red_bandit_11@hotmail.com>
2022-11-09 23:43:42 -0500

    removed from ./

--- src/app/pipes/pipes.module.ts
+++
@@ -1,8 +0,0 @@
-import { PhoneNumberPipe } from './phonenumber';
-import { NgModule } from '@angular/core';
-
-@NgModule({
- declarations: [PhoneNumberPipe],
- exports: [PhoneNumberPipe]
-})
-export class PipesModule { }
--- src/util.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-module.exports = function (ctx){
-    fs.copyFile('src/manifest.json', 'www/manifest.json', (error)=> console.error('Error copying files'));
-    return 0;
-}
